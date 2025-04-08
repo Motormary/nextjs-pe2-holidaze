b42bc7988904dbbb392a1b7f14c874784944a2c0
Mathias <mat.moen@gmail.com>
2025-04-08 22:41:46 +0200

    fix: test

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,6 @@
 import { Button } from "@/components/ui/button"
 
-type asdf = {
-  asd: string
-}
-
-export default function Home({ asd }: asdf) {
+export default function Home() {
   return (
     <div>
       <Button>Hello</Button>
