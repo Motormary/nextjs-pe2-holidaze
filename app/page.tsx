24297f4591aca5f4600b23b2ad4d342cff812ce4
Mathias <mat.moen@gmail.com>
2025-05-06 10:59:31 +0200

    feat: venue grid layout

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,10 @@ export default function Home() {
     <main className="container grow px-4">
       <h1>Home</h1>
       <div>bar</div>
-      <div>
+      <div className="grid grid-cols-[repeat(auto-fill,minmax(270px,1fr))] gap-5">
+        <BasicVenueCard />
+        <BasicVenueCard />
+        <BasicVenueCard />
         <BasicVenueCard />
         <BasicVenueCard />
       </div>
